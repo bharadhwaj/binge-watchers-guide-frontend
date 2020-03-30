ea9a237d9392d48c4d412c90819f853798c20021
Bharadhwaj CN <bharadhwaj@getbasis.co>
2020-03-31 00:40:01 +0530

    Fix the issue with rejecting URLs

--- src/constants/regex.js
+++ src/constants/regex.js
@@ -1,4 +1,4 @@
 export const USERNAME = /^[a-z][a-z'-_.]{2,}$/i;
 export const USERNAME_TYPING = /^[a-z][a-z'-_.]*$/i;
 
-export const URL = /[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,24}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)?/gi;
+export const URL = /((?:https?:\/\/)[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,24}\b(?:[-a-zA-Z0-9()@:%_+.~#?&\/\/=]*)?)/gi;
